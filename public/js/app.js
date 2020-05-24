c5e63405f4e1ccc473b74fb12036811ab2dd67b6
nicngo <nicngo@cisco.com>
2020-05-24 16:22:06 -0700

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,18 @@
+console.log('client side js file loaded')
+
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
+        console.log(data)
+    })
+})
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+messageOne.textContent = 'From JavaScript'
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    console.log('Location selected: ' + location)
+})
---
+++ src/app.js
@@ -0,0 +1,91 @@
+import path from 'path'
+import express from "express"
+import hbs from "hbs"
+
+const __dirname = path.resolve()
+console.log("Path.resolve: " + __dirname)
+ 
+const publicDirectoryPath = path.join(__dirname, '/public')
+console.log("public dir: " + publicDirectoryPath)
+
+const viewPath= path.join(__dirname, '/templates/views')
+console.log("viewPath: " + viewPath)
+const partialsPath= path.join(__dirname, '/templates/partials')
+console.log("partialsPath: " + partialsPath)
+
+const app = express()
+//This is to serve all pages from public
+app.use(express.static(publicDirectoryPath))
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+console.log('viewPATH ' + viewPath)
+hbs.registerPartials(partialsPath)
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather Page Header',
+        name: 'Nicholas Ngo'
+    })
+})
+
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Page Header ',
+        name: 'Nicholas Ngo'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page Header',
+        name: 'Nicholas Ngo'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if(!req.query.address){
+        return res.send({
+            error: 'You must provide an address to search'
+        })
+    }
+    const target_address = req.query.address
+
+    res.send({
+        forcast: 'It is snowing',
+        location: req.query.address 
+    })
+})
+
+app.get('/product', (req, res) => {
+    if (!req.query.search){
+        return res.send({
+            error: 'You must provide a search term' 
+        })
+    }
+
+    console.log(req.query)
+    res.send({
+        product: []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Nicholas Ngo',
+        errorMessage: 'Help Artical NOT FOUND !!!'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Nicholas Ngo',
+        errorMessage: 'Page NOT FOUND !!!'
+    })
+})
+
+ app.listen(3000, () => {
+     console.log('Server is up on port 3000')
+ })
